e5f2f098e7f1f097053d5ed4799486caefa3f3b0
James Bowes <jbowes@repl.ca>
2022-10-05 16:59:15 -0300

    Update copyright year

--- benchmark_test.go
+++ benchmark_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2021 James Bowes. All rights reserved.
+// Copyright (c) 2021-2022 James Bowes. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2021 James Bowes. All rights reserved.
+// Copyright (c) 2021-2022 James Bowes. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -19,27 +19,27 @@
 // (https://semver.npmjs.com/). This behaviour is only what exists now; feature
 // requests or contributions are welcome to expand on it!
 //
-// Normalized Constraint forms
+// # Normalized Constraint forms
 //
 // Internally during parsing, and when returning string representations,
 // Constraints are normalized in their representation.
 //
 // Rules for normalization:
-//  - replace the empty match-any value "" with *
-//  - replace any whitespace with a single space
-//  - surround `||` with a single space on each side, if missing
-//  - remove '='.
-//  - replace partial semver (2.0) with wildcard version (2.0.x)
-//  - replace partial wildcard (2.x) with full wildcard (2.x.x)
-//    special case: leave full wildcard (*, x, X) as-is, but convert to *.
-//  - for wildcard with leading op (including tilde and semver), replace
-//    wildcards with 0.
-//  - replace remaining wildcard semver (including previous partial) with
-//    >= and < clauses (eg 2.x.x becomes >=2.0.0 < 3.0.0)
-//  - replace tilde with >= and < clauses (eg ~2.1.1 becomes >=2.1.1 < 2.2.0)
-//  - replace caret with >= and < clauses (eg ^2.1.1 becomes >=2.1.1 < 3.0.0)
-//  - replace hyphenated ranges with >= and <= clauses
-//    (eg 2.1.0 - 2.2.0 becomes >=2.1.0 <=2.2.0)
+//   - replace the empty match-any value "" with *
+//   - replace any whitespace with a single space
+//   - surround `||` with a single space on each side, if missing
+//   - remove '='.
+//   - replace partial semver (2.0) with wildcard version (2.0.x)
+//   - replace partial wildcard (2.x) with full wildcard (2.x.x)
+//     special case: leave full wildcard (*, x, X) as-is, but convert to *.
+//   - for wildcard with leading op (including tilde and semver), replace
+//     wildcards with 0.
+//   - replace remaining wildcard semver (including previous partial) with
+//     >= and < clauses (eg 2.x.x becomes >=2.0.0 < 3.0.0)
+//   - replace tilde with >= and < clauses (eg ~2.1.1 becomes >=2.1.1 < 2.2.0)
+//   - replace caret with >= and < clauses (eg ^2.1.1 becomes >=2.1.1 < 3.0.0)
+//   - replace hyphenated ranges with >= and <= clauses
+//     (eg 2.1.0 - 2.2.0 becomes >=2.1.0 <=2.2.0)
 //
 // Normalized constraints are not canonical. Two Constraints can have different
 // normalized forms but still be equivalent.
